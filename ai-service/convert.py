1dbd8001e11da15c4cb642b3fdec0fa1e3a5f984
DEVANG SINGH <devang9890@gmail.com>
2026-02-01 22:20:13 +0530

    updated

--- ai-service/convert.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-with open("face.jpg", "rb") as f:
-    encoded = base64.b64encode(f.read()).decode()
-
-with open("image.txt", "w") as f:
-    f.write(encoded)
-
-print("Base64 saved to image.txt")
